c033009cdd29fbe16285fa13cdd60d83de400e57
AviralSheoran <aviralsheoran.2002@gmail.com>
2022-04-22 22:32:59 +0530

    STL Overview

---
+++ STL.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    /* 
+    * STL Parts - 
+                1. Containers
+                2. Iterators
+                3. Algorithms
+                4. Functors
+                
+    * 1. Containers - 
+            TODO: Nested Containers
+            -> Sequential
+                    a). Vectors
+                    b). Stack
+                    c). Queue
+                    d). Pair (Not a Container)
+
+            -> Unordered (Used in Hashing Problems)
+                    a). Unordered Map
+                    b). Unordered Set
+                    
+            -> Ordered (Value are saved in ordered manner)
+                    a). Maps
+                    b). Multimap
+                    c). Set
+                    d). Multiset
+
+    * 2. Iterators (Similar to Pointers) - 
+                    -> Point to memory address of Containers
+                    -> begin() , end() iterator
+                    -> Continuity and discontinuity for Containers
+                    ? Syntax -> vector <int>:: iterator it;
+
+    * 3. Algorithms - 
+                    -> Upper Bound and Lower Bound
+                    -> Sort (Comparator)
+                    -> max_element and min_element
+                    -> accumulate - to directly find the sum of array
+                    -> reverse
+                    -> count 
+                    -> find - position of element 
+                    -> next permutaion
+                    -> prev permutaion
+                    and so on...
+
+    ! WE DON"T NEED TO USE THIS👇
+    * 4. Functors - Classes which can act as functions  
+
+    */
+    
+
+    return 0;
+}
